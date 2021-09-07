d19c83305d0310a4dfd34af980be765fc0887194
ti777 <tifany090295@hotmail.com>
2021-09-07 20:58:14 +0200

    Init

---
+++ roman/src/test/java/no/kristiania/RomanNumbersTest.java
@@ -0,0 +1,17 @@
+package no.kristiania;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RomanNumbersTest {
+
+    private final RomanNumbers romanNumbers;
+
+    public RomanNumbersTest() {
+        romanNumbers = new RomanNumbers();
+    }
+
+    @Test
+    public void test() {
+        Assertions.assertEquals(0,0);
+    }
+}
